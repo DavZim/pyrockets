8287beff31fc4a6b0063a7c952aac09ed33ac9e5
DavZim <david.zimmermann@zu.de>
2017-04-06 15:24:33 +0200

    03 - Rectangle Move and Rotate

--- Rectangle.py
+++ Rectangle.py
@@ -9,12 +9,66 @@ class Rectangle(object):
         self.y = y
         self.l = l
         self.w = w
+        self.theta = theta
         # calculate the points from the x,y,l,w,theta information
         self.pts = np.array([[x + w / 2, y + l / 2], [x + w / 2, y - l / 2],
                             [x - w / 2, y - l / 2], [x - w / 2, y + l / 2]])
         # rotate the points to the given the    ta
-        # self.rotate_to(theta)
+        self.rotate_to(theta)
 
     # returns the points
     def get_pts(self):
         return self.pts
+
+    # moves the rectangle by given values
+    def move_by(self, delta_x, delta_y):
+        delta = np.array([delta_x, delta_y])
+        self.x += delta_x
+        self.y += delta_y
+        self.pts += delta
+
+    # moves the rectangle to given coordinates
+    def move_to(self, x, y):
+        self.x = x
+        self.y = y
+        self.pts = self.pts - np.apply_along_axis(np.mean, 0, self.pts) + np.array([x, y])
+
+    # moves the rectangle a certain amount in the direction of heading
+    def move_forwards(self, amt):
+        amt_vec = np.array([0, amt])
+        mv_vec = self.rotate_internal(amt_vec, self.theta, 0, 0)
+        self.move_by(mv_vec[0], mv_vec[1])
+
+    # rotate the rectangle to a given theta
+    def rotate_to(self, theta):
+        self.pts = self.rotate_internal(self.pts, theta=theta - self.theta, x=self.x, y=self.y)
+        self.theta = theta % 360
+
+    # rotate the rectanlge by a given theta
+    def rotate_by(self, theta):
+        self.pts = self.rotate_internal(self.pts, theta=theta, x=self.x, y=self.y)
+        self.theta = (self.theta + theta) % 360
+
+    # rotate the rectangle around a given set of coordinates
+    def rotate_around(self, theta, x, y):
+        self.theta += theta
+        self.pts = self.rotate_internal(self.pts, theta, x, y)
+        mid = np.apply_along_axis(np.mean, 0, self.pts)
+        self.x = mid[0]
+        self.y = mid[1]
+
+    # internal method that actually rotates the points etc. is used by the other rotate_x functions
+    @staticmethod
+    def rotate_internal(pts, theta, x, y):
+        theta_rad = theta * np.pi / 180
+        # get the average values
+        middle = np.array([x, y])
+        # shift to rotation point
+        pts_0 = pts - middle
+        # rotate by matrix
+        rot_matrix = np.array([[np.cos(theta_rad), -np.sin(theta_rad)], [np.sin(theta_rad), np.cos(theta_rad)]])
+        # actually rotate the pts
+        pts_0_rotated = np.dot(pts_0, rot_matrix)
+        # shift back to original area
+        pts_rotated = pts_0_rotated + middle
+        return pts_rotated
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((1000,1000), 0, 32)
     screen.fill((0, 0, 0))  # fill black
     pygame.init()
+    # FPS = Frames Per Second
+    FPSCLOCK = pygame.time.Clock()
+
 
     # draw the target circle
     pygame.draw.circle(screen, (255, 0, 0), (500,100), 40, 0)
@@ -36,7 +39,31 @@ if __name__ == "__main__":
 
     # game routine: loop that runs until we quit [x]
     while True:
+        # check exit conditions
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+        # redraw the screen elements
+        screen.fill((0,0,0))
+        pygame.draw.polygon(screen, (255, 255, 255), obs1.get_pts())
+        scoretext = font.render("Hello World, we are in round %d" % (0), 1, (255, 255, 255))
+        screen.blit(scoretext, scoretext.get_rect(center=(500,500)))
+        st2 = font.render("below is an obstacle, at the bottom is a rocket, and above is the target",
+            1, (255, 255, 255))
+        screen.blit(st2, scoretext.get_rect(center=(350,600)))
+
+        # move the rocket upwards by 10 units
+        rocket1.move_forwards(-10)
+        # check that the rocket is not outside of the screen
+        if rocket1.y < 0:
+            rocket1.move_to(500, 1000)
+        # draw the rocket
+        pygame.draw.polygon(screen, (0,0,255), rocket1.get_pts())
+
+        # update the display
+        pygame.display.update()
+
+        # wait so long that we achieve 50 FPS
+        FPSCLOCK.tick(50)
